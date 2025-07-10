430e997892f9fca5bbd2f982bd1540f5daafc563
drclcomputers <andreidelatimisoara@gmail.com>
2025-07-10 22:57:09 +0300

    New features

--- cmd/update.go
+++ cmd/update.go
@@ -5,7 +5,10 @@ package cmd
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"os/exec"
+	"path/filepath"
 	"runtime"
 
 	"github.com/spf13/cobra"
@@ -19,9 +22,24 @@ var updateCmd = &cobra.Command{
 		if runtime.GOOS == "windows" {
 			fmt.Println("The included bash script only works on Linux and UNIX-like OSes. For Windows, update manually.")
 		} else {
+			fmt.Println("Starting update...")
 			err := os.Chmod("update.sh", 0755)
 			if err != nil {
 				fmt.Println("Failed to make script executable:", err)
+			} else {
+				fmt.Println("Indexing the update script...")
+			}
+			exePath, err := os.Executable()
+			if err != nil {
+				log.Fatalf("Failed to get executable path: %v", err)
+			}
+			exeDir := filepath.Dir(exePath)
+			scriptPath := filepath.Join(exeDir, "update.sh")
+			cmd := exec.Command(scriptPath)
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+			if err = cmd.Run(); err != nil {
+				log.Fatal(err)
 			}
 		}
 	},
