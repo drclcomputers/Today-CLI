f6d0c56f3b786759c87a064fc831cc5feae63c4d
drclcomputers <andreidelatimisoara@gmail.com>
2025-07-11 15:36:02 +0300

    Bug fixes

--- cmd/update.go
+++ cmd/update.go
@@ -35,7 +35,7 @@ var updateCmd = &cobra.Command{
 			} else {
 				fmt.Println("Indexing the update script...")
 			}
-			cmd := exec.Command("sudo ." + scriptPath)
+			cmd := exec.Command(scriptPath)
 			cmd.Stdout = os.Stdout
 			cmd.Stderr = os.Stderr
 			if err = cmd.Run(); err != nil {
